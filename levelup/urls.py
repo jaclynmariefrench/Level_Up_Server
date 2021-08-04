635b03cfcc5dae3db120d72c70f96a7c28873073
Jaclyn French <jaclynmariefrench@gmail.com>
2021-08-04 10:36:53 -0500

    finished ch7

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,8 +1,21 @@
+from levelupapi.views.game import GameView
 from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
+from rest_framework import routers
+from levelupapi.views import GameTypeView, GameView
+from django.contrib import admin
+
+
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'games', GameView, 'game')
+
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
