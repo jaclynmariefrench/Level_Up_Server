81314de3b5c17d2f789e2cab46f7fe4b4f1e0c24
Jaclyn French <jaclynmariefrench@gmail.com>
2021-08-13 11:42:32 -0500

    finished profile backend

--- levelup/urls.py
+++ levelup/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
 from rest_framework import routers
-from levelupapi.views import GameTypeView, GameView, EventView
+from levelupapi.views import GameTypeView, GameView, EventView, Profile
 from django.contrib import admin
 
 
@@ -12,7 +12,7 @@ router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
 router.register(r'events', EventView, 'event')
-
+router.register(r'profile', Profile, 'profile')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
